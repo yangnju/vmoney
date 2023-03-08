201076f07910727379a2b5a821ca4e1e1a2807e4
cstark <449134875@qq.com>
2023-03-08 15:39:40 +0800

    增加三个页面，增加他们的路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <div id="nav">
+      <router-link to="/">Money</router-link> |
+      <router-link to="/labels">Labels</router-link> |
+      <router-link to="/statistics">Statistics</router-link>
+    </div>
   </div>
 </template>
 
---
+++ src/components/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+<div>statistics.vue</div>
+</template>
+
+<script>
+export default {
+  name: "Statistics"
+}
+</script>
+
+<style scoped>
+
+</style>
