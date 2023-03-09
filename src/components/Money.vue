1a8351d0ea0fbf91db54aa8192308ac89d9d4871
cstark <449134875@qq.com>
2023-03-09 14:50:01 +0800

    修改money页布局，把导航移动到底部。

--- src/components/Money.vue
+++ src/components/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <div>money.vue</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>money.vue</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,6 +13,17 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid red;
+  background-color: #42b983;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.content{
+  background-color: coral;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
