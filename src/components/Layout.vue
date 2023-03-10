bf6762cc76b68f992e965e61b69fb1d0403565aa
cstark <449134875@qq.com>
2023-03-10 14:28:44 +0800

    添加svg loader，为导航增加图片

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -16,13 +16,11 @@ export default {
 <style lang="scss" scoped>
 .nav-wrapper{
   border: 1px solid red;
-  background-color: #42b983;
   height: 100vh;
   display: flex;
   flex-direction: column;
 }
 .content{
-  background-color: coral;
   overflow: auto;
   flex-grow: 1;
 }
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Money</router-link> |
-    <router-link to="/labels">Labels</router-link> |
-    <router-link to="/statistics">Statistics</router-link>
+  <div>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      记账
+    </router-link>|
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      标签
+    </router-link>|
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>统计
+    </router-link>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import x from '@/assets/icons/money.svg'
+import y from '@/assets/icons/label.svg'
+import z from '@/assets/icons/statistics.svg'
+console.log(x)
+console.log(y)
+console.log(z)
 export default {
   name: "Nav"
 }
 </script>
-
-<style scoped>
-
-</style>
